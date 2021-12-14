a48f69b542482f5cb37afdc42628558e1a86c8f8
Marcelo Santos <marcelo.costtasantos@gmail.com>
2021-12-14 19:29:52 -0300

    .watchman com erro

--- index.vr.js
+++ index.vr.js
@@ -12,6 +12,20 @@ export default class MyHome extends React.Component {
     return (
       <View>
         <Pano source={asset('star.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          OK
+        </Text>
       </View>
     );
   }
